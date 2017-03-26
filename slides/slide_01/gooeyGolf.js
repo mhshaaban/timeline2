d9bed5fe03748d9e1a9ed1b6fb4cacc5c02d1678
mhshaaban <m.h.shaaban82@gmail.com>
2017-03-26 02:09:46 +0100

    Update

--- slides/slide_01/gooeyGolf.js
+++ slides/slide_01/gooeyGolf.js
@@ -33,9 +33,9 @@ pt.gooeyGolf.init = function() {
 	///////////////////////////////////////////////////////////////////////////
 
     
-    svg.append("image")
+    svg.append('image')
     	.attr("class", "image")
-	.attr("x", width/5)
+	.attr("x", width/4.5)
 	.attr("y", -100)
 	.attr("xlink:href","/timeline2/img/yalbugha.png")
 	.style("opacity", 0);	
